56fd298f0afa3c92f5e03be2e1a2b7f2a9ff5f3f
Omykrönn <omykronn.player@gmail.com>
2021-10-27 17:18:26 +0200

    TP Paroi - Fonction testparoipermeable()
    - Ajout de la fonction testparoipermeable() qui teste la perméabilité de la paroi par simulation
    - Amélioration de la fonction show() pour qu'elle affiche les cellules humides
    - Ajout de la variable prof1, contenant la paroi-exemple n°1

--- tp_paroi.py
+++ tp_paroi.py
@@ -1,4 +1,8 @@
 from random import randrange
+import numpy as np
+
+
+prof1 = [[0,1,1,1,0,0,0],[1,1,0,1,0,0,0],[0,1,0,1,0,0,0],[0,0,0,1,0,1,1],[0,1,0,1,0,1,0],[0,1,0,1,0,1,0],[0,0,0,1,1,1,0],[0,1,0,0,0,0,0]]
 
 
 def creeparoi(l: int, c: int, p: float):
@@ -24,6 +28,56 @@ def creeparoi(l: int, c: int, p: float):
     return paroi
 
 
+def testparoipermeable(paroi: list):
+    """
+    Test de la perméabilité de la paroi par simulation
+
+    :param list paroi: Paroi à tester
+    :return bool: Booléen en fonction de la perméabilité de la paroi
+    """
+    height, width = np.shape(paroi)[:2]
+
+    # On mouille la surface extrème-gauche
+    for i in range(height):
+        if paroi[i][0] == 1:
+            paroi[i][0] = 2
+
+    show(paroi)
+
+    # On étend l'humidité dans la paroi
+    for j in range(1, width):
+        for i in range(height):
+            if paroi[i][j] == 1 and paroi[i][j - 1] == 2:
+                # --- On étend sur la même colonne vers le haut
+                a = 0
+
+                while i - a >= 0 and paroi[i - a][j] == 1:
+                    paroi[i - a][j] = 2
+                    a += 1
+
+                    # show(paroi)  # Ligne à décommenter pour voir l'évolution
+
+                # --- On étend sur la même colonne vers le bas
+                a = 1  # La cellule (i; j) est déjà mouillée
+
+                while i + a < height and paroi[i + a][j] == 1:
+                    paroi[i + a][j] = 2
+                    a += 1
+
+                    # show(paroi)  # Ligne à décommenter pour voir l'évolution
+
+    # Test de la permeabilité
+
+    permeable = False
+    i = 0
+
+    while i < height and not permeable:
+        permeable = paroi[i][-1] == 2
+        i += 1
+
+    return permeable
+
+
 # ------ FONCTION EN PLUS ------
 
 
@@ -38,6 +92,9 @@ def show(paroi: list):
         for cell in line:
             if cell == 0:
                 print("█", end="")
-            else:
+            elif cell == 1:
                 print("░", end="")
+            else:
+                print("×", end="")
         print("")
+    print("\n")
